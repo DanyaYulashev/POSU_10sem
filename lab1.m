787bc79b857751109355d5ab973ce5d52f2a76c5
DanyaYulashev <crystalcat314@gmail.com>
2026-03-14 15:54:19 +0300

    add gitignore

--- lab1.m
+++ lab1.m
@@ -5,7 +5,7 @@ fun = @(t,x) [2*x(2); -1*x(1) + 0.5*x(3); -2*x(1)-4*x(2)-3*x(3)];
 [t,X] = ode45(fun, [0 t_end], [x10, x20, x30]);
 
 A = [0,2,0;-1,0,0.5;-2,-4,-3];
-eigenvals = eig(A);
+
 T = t;
 figure('color','white');
 grid on; hold on;
